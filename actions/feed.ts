9c7654a7f03846ee6387b7ff549975fe40dd56fa
ikbal arslan <ikbalarslandev@gmail.com>
2025-04-17 21:08:10 +0300

    feeds improve

--- actions/feed.ts
+++ actions/feed.ts
@@ -76,6 +76,61 @@ const generateActionFeed = async ({
   );
 };
 
+const generateServiceFeed = async ({
+  filenameTimestamp,
+  feedsDir,
+  timestamp,
+}: {
+  filenameTimestamp: string;
+  feedsDir: string;
+  timestamp: string;
+}) => {
+  const products = await prisma.product.findMany({
+    include: {
+      business: true,
+    },
+  });
+
+  const serviceFeed = products.map((p) => ({
+    merchant_id: `business-${p.businessId}`,
+    service_id: `service-${p.id}`,
+    name: p.name,
+    description: `${p.name}`,
+    price: {
+      currency_code: "TRY",
+      units: Math.floor(p.price),
+      nanos: Math.round((p.price % 1) * 1e9),
+    },
+    prepayment_type: "NO_PREPAYMENT",
+    action_link: {
+      url: `https://www.barber.hamampass.com/rezervasyon/${p.businessId}?productId=${p.id}`,
+      action_link_type: "BOOK",
+    },
+    duration: {
+      seconds: p.duration * 60,
+    },
+  }));
+
+  const serviceFilename = `glam.service.v0-${filenameTimestamp}.json`;
+  fs.writeFileSync(
+    path.join(feedsDir, serviceFilename),
+    JSON.stringify(serviceFeed, null, 2)
+  );
+
+  const serviceDescriptor = {
+    name: "glam.service.v0",
+    generation_timestamp: timestamp,
+    files: [serviceFilename],
+  };
+  fs.writeFileSync(
+    path.join(
+      feedsDir,
+      `glam.service.v0-${filenameTimestamp}.filesetdesc.json`
+    ),
+    JSON.stringify(serviceDescriptor, null, 2)
+  );
+};
+
 const generateFeeds = async () => {
   const timestamp = new Date().toISOString();
   const filenameTimestamp = timestamp
@@ -97,6 +152,12 @@ const generateFeeds = async () => {
   });
 
   await generateActionFeed({
+    filenameTimestamp,
+    feedsDir,
+    timestamp,
+  });
+
+  await generateServiceFeed({
     filenameTimestamp,
     feedsDir,
     timestamp,
