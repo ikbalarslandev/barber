9eada148ef341a74ad530ef3e94730cf85bd87ff
ikbal arslan <ikbalarslandev@gmail.com>
2025-04-27 13:57:59 +0300

    bug fix

--- app/(customer)/rezervasyon/[id]/page.tsx
+++ app/(customer)/rezervasyon/[id]/page.tsx
@@ -1,32 +1,6 @@
 import { getProducts } from "@/actions/getProducts";
 import Booking from "@/components/pages/booking";
 import { TBusiness } from "@/prisma/types";
-import { Metadata } from "next";
-
-type BookingPageProps = {
-  params: { id: string };
-};
-
-// Dynamic metadata
-export async function generateMetadata({
-  params,
-}: BookingPageProps): Promise<Metadata> {
-  const { id } = params;
-
-  const business = (await getProducts(id)) as TBusiness | null;
-
-  if (!business) {
-    return {
-      title: "Hamampass - Not Found",
-      description: "Business not found",
-    };
-  }
-
-  return {
-    title: `Hamampass - ${business.name}`,
-    description: `Booking page for ${business.name}`,
-  };
-}
 
 const BookingPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
